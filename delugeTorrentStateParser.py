c3673489dca69ae763dabd7222ed8b925177639a
morski <marten.gustavsson@netlight.com>
2018-10-23 23:42:57 +0300

    Broke up code in functions and added support for arguments

--- delugeTorrentStateParser.py
+++ delugeTorrentStateParser.py
@@ -1,4 +1,5 @@
 import json
+import sys, getopt
 
 class TorrentData(object):
     def toJSON(self):
@@ -10,45 +11,67 @@ class TorrentData(object):
     url = ""
 
 
-filepath = 'torrents.state'  
-with open(filepath) as fp:  
-    line = fp.readline()
-    cnt = 0
-    torrents = []
-    hash, name, path = False, False, False
-    urlString, nameString, pathString, hashString = "", "", "", ""
-    torrentData = TorrentData()
-    while line:
-        if hash:
-            torrentData.hash = line[2:-2]
-            hash = False
-        if name:
-            torrentData.name = line[2:-2]
-            name = False
-        if path:
-            torrentData.path = line[2:-2]
-            path = False
-
-
-        if "http" in line.strip():
-            torrentData.url = line[2:-2] + " - "
-        if line.strip() == "asg18":
-            name = True
-        if line.strip() == "sg25":
-            hash = True
-        if line.strip() == "sg21":
-            path = True
-
-        if torrentData.url != "" and torrentData.name != "" and torrentData.path != "" and torrentData.hash != "":
-            torrents.append(torrentData)
-            torrentData = TorrentData()
-        line = fp.readline()
-
+def saveTorrentDataToJson(torrentData, outputFile):
+    outputFile = sys.argv
     with open('torrents.json', 'w') as outfile:
         outfile.write("[\n")
         jsonTorrentsList = []
-        for torrent in torrents:
+        for torrent in torrentData:
             jsonTorrentsList.append(torrent.toJSON())
             outfile.write(torrent.toJSON() + ",\n")
     	outfile.write(",".join(jsonTorrentsList))
         outfile.write("]\n")
+
+def main(argv):
+    inputFileName = 'torrents.state'
+    outputFileName = 'torrents.json'
+
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print 'test.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputFileName = arg
+        elif opt in ("-o", "--ofile"):
+            outputFileName = arg
+
+    with open(inputFileName) as stateFile:  
+        line = stateFile.readline()
+        torrents = []
+        hash, name, path = False, False, False
+        torrentData = TorrentData()
+        while line:
+            if hash:
+                torrentData.hash = line[2:-2]
+                hash = False
+            if name:
+                torrentData.name = line[2:-2]
+                name = False
+            if path:
+                torrentData.path = line[2:-2]
+                path = False
+
+
+            if "http" in line.strip():
+                torrentData.url = line[2:-2] + " - "
+            if line.strip() == "asg18":
+                name = True
+            if line.strip() == "sg25":
+                hash = True
+            if line.strip() == "sg21":
+                path = True
+
+            if torrentData.url != "" and torrentData.name != "" and torrentData.path != "" and torrentData.hash != "":
+                torrents.append(torrentData)
+                torrentData = TorrentData()
+            line = stateFile.readline()
+
+    saveTorrentDataToJson(torrents, outputFileName)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
