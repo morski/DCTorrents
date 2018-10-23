74bf7cffa6f4659a02f53cb4d86bbee6c434ce54
morski <marten.gustavsson@netlight.com>
2018-10-24 00:42:35 +0300

    Added compare function for two JSON files

--- delugeTorrentStateParser.py
+++ delugeTorrentStateParser.py
@@ -1,5 +1,6 @@
 import json
 import sys, getopt
+import difflib
 
 class TorrentData(object):
     def toJSON(self):
@@ -22,24 +23,7 @@ def saveTorrentDataToJson(torrentData, outputFile):
     	outfile.write(",".join(jsonTorrentsList))
         outfile.write("]\n")
 
-def main(argv):
-    inputFileName = 'torrents.state'
-    outputFileName = 'torrents.json'
-
-    try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-    except getopt.GetoptError:
-        print 'test.py -i <inputfile> -o <outputfile>'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print 'test.py -i <inputfile> -o <outputfile>'
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputFileName = arg
-        elif opt in ("-o", "--ofile"):
-            outputFileName = arg
-
+def parseTorrentStateFile(inputFileName):
     with open(inputFileName) as stateFile:  
         line = stateFile.readline()
         torrents = []
@@ -70,8 +54,52 @@ def main(argv):
                 torrents.append(torrentData)
                 torrentData = TorrentData()
             line = stateFile.readline()
+        return torrents
 
-    saveTorrentDataToJson(torrents, outputFileName)
+def compareStateJsonFiles(fileA, fileB):
+    with open(fileA,'r') as f1, open(fileB,'r') as f2:
+        diff = difflib.ndiff(f1.readlines(),f2.readlines())    
+        for line in diff:
+            if line.startswith('-'):
+                sys.stdout.write(line)
+            elif line.startswith('+'):
+                sys.stdout.write('\t\t'+line)  
+
+def main(argv):
+    inputFileName = []
+    inputFileName.append('torrents.state')
+    outputFileName = []
+    outputFileName.append('torrents.json')
+    
+    parseMode = True
+    compareMode = False
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:c",["ifile=","ofile=","comparemode"])
+        print(args)
+    except getopt.GetoptError:
+        print 'test.py -i <inputfile> -o <outputfile> -c (comparemode)'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputFileName.append(arg)
+        elif opt in ("-o", "--ofile"):
+            outputFileName.append(arg)
+        elif opt in ("-c", "--comparemode"):
+            compareMode = True
+            parseMode = False
+
+    if parseMode:
+        torrents = parseTorrentStateFile(inputFileName[1] if len(inputFileName) > 1 else inputFileName[0])
+        saveTorrentDataToJson(torrents, outputFileName[1] if len(outputFileName) > 1 else outputFileName[0])
+    
+    if compareMode:
+        if len(outputFileName) < 3:
+            print("Filenames missing. Use two -i to define json files")
+            sys.exit(2)
+        compareStateJsonFiles(outputFileName[1], outputFileName[2])
 
 if __name__ == "__main__":
    main(sys.argv[1:])
